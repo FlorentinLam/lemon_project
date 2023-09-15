d356538e0589c80133db099c242642735144742c
Florentin Lamour <lamour.florentin@gmail.com>
2023-09-15 17:23:23 +0200

    Logs Recherche

--- php/Controller/SearchController.php
+++ php/Controller/SearchController.php
@@ -11,7 +11,10 @@ class SearchController {
 
     public function renderSearchPage() {
         $query = $_GET['keyword'] ?? null;
-    
+
+        $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : 'anonyme';
+        $this->logSearch($user_id, $query);
+            
         if ($query) {
             $movies = $this->apiService->searchMovies($query);
         } else {
@@ -19,6 +22,26 @@ class SearchController {
         }
         
         include __DIR__ . '/../../views/movie/search.html';
+    }
+
+    public function logSearch($user_id, $query) {
+
+        $dateTime = date("Y-m-d H:i:s");
+        $pdo = connect();
+        $stmt = $pdo->prepare("SELECT first_name FROM user WHERE id = :user_id");
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->execute();
+        $username = $stmt->fetchColumn();
+        
+        // Formattez le message du journal
+        $logMessage = "[$dateTime] Utilisateur: $username, Recherche: $query\n";
+        
+        // Chemin vers le fichier journal
+        $logFile = 'logs/search.log';
+
+        // Écrivez le message dans le fichier journal
+        file_put_contents($logFile, $logMessage, FILE_APPEND);
+
     }
     
 }
